389b2509f06d189dc6c316d643f45fa240974977
Venkat Rajagopalan <Venkat Rajagopalan>
2015-10-04 01:19:34 -0400

    Now retrieving the policy at the “next” state.
    Added the action as a feature dimension
    
    Action a added to phi_s and pi_s_prime added to  s_prime

--- LSTDQ.py
+++ LSTDQ.py
@@ -22,13 +22,13 @@ with open('generated_episodes_3000.csv') as csv_file:
 # Helper functions
 def create_event_iterator(data):
     """
-        Function that returns the next (s, a, r, s') pair from the input data one by one every time you call it.
+        Function that returns the next (s, a, r, s', a(s')) pair from the input data one by one every time you call it.
     """
-    event_length = 9 + 9 + 2
+    event_length = 9 + 9 + 2 + 1
     state_length = 9 + 2
     num_states = 24
     for episode in data:
-        # Start at the beginning and keep looking at a net length of len(s) + len(a) + len(r) + len(s') points
+        # Start at the beginning and keep looking at a net length of len(s) + len(a) + len(r) + len(s') + len(a(s')) points
         # Each time, we increment our start position by s+a+r = 11 points
         curr_state = 0
         while curr_state < num_states:
@@ -39,8 +39,9 @@ def create_event_iterator(data):
                 s = datum[:9].astype(np.float)
                 a = 1.0 if datum[9:10]=='true' else 0.0
                 r = np.asscalar(datum[10:11].astype(np.float))
-                s_prime = datum[11:].astype(np.float)                
-                yield s, a, r, s_prime
+                s_prime = datum[11:20].astype(np.float)
+                pi_s_prime =  1.0 if datum[20:21]=='true' else 0.0               
+                yield s, a, r, s_prime, pi_s_prime
             # There's a problem if a data field is 'NA' - Not entirely sure what do in that case so for now I'm just ignoring
             # those data points
             except ValueError:
@@ -50,11 +51,14 @@ def create_event_iterator(data):
 # Instantiation of the Ordinary Least Squares class
 ols = linear_model.LinearRegression()
 
-# at present, our basis is N^d - where d is 9
-k = 9
+# at present, our basis is N^d - where d is 9 (features) + 1 (action)
+k = 10
 
-# the "A" Matrix (kxk)
-A = np.zeros([k, k], dtype=float)
+# to avoid singularities, we start off A with a small delta along the diagonal
+delta = 1e-03
+
+# the "A" Matrix (kxk) 
+A = delta*np.eye(k)
 
 # the b vector
 b = np.zeros([k, 1], dtype=float)
@@ -62,18 +66,18 @@ b = np.zeros([k, 1], dtype=float)
 # the discount factor gamma
 gamma = 0.9
 
-# generate the object that will produces us the (s,a,r,s') tuple
+# generate the object that will produces us the (s,a,r,s', pi_s_prime) tuple
 event_iterator = create_event_iterator(data)
 
 print "Building the matrix A and the vector b"
 
 # iterate through the tuple
-for s, a, r, s_prime in event_iterator:
+for s, a, r, s_prime, pi_s_prime in event_iterator:
     
     # In future implementations, we can have a different basis such as RBF or a polynomial for our features
     # In the current implementation, we take the given features as the basis 
-    phi_s = np.reshape(s, (k,1))
-    phi_s_prime = np.reshape(s_prime, (k,1))
+    phi_s = np.reshape(np.append(s,a), (k,1))
+    phi_s_prime = np.reshape(np.append(s_prime, pi_s_prime), (k,1))
     
     # Update A - here, we add to A, a Rank 1 matrix formed by phi(s) and (phi(s) - gamma*phi(s'))
     A = A + np.outer(phi_s, phi_s - gamma*phi_s_prime)
