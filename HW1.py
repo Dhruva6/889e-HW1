Fixed bug in OMP-TD. Need to confirm whether computation is correct

HW1.py
@@ -76,7 +76,7 @@ if options.testData == False:
             # console log
             #  the initial policy executed at s'
             current_pi = np.reshape(sarsa[:,4], (len(sars),1))
-            OMP_TD(sars, 100)
+            OMP_TD(sars, 6)
             current_pi, w_pi, current_value = LSPI(sars, current_pi, gamma)
         else:
             print "Running FVI on *ALL* the training data with gamma {0:.3f}".format(gamma)

OMP.py
@@ -5,7 +5,7 @@ def OMP_TD(sars, beta, gamma = 0.99):
     k = 9
     I = set()
     w_pi = np.zeros((k, 1))
-    c = [beta for beta in range(k)]
+    c = [beta+1 for i in range(k)]
     j = 0
     n = len(sars)
     
@@ -15,17 +15,17 @@ def OMP_TD(sars, beta, gamma = 0.99):
     phi_s_prime = np.array([np.array(elem) for elem in sars[:,3]])
     phi_s_prime = np.reshape(phi_s_prime, (n, k))
 
-    R = sars[:, 2]
-    while(len(I)!=n and c[j] <= beta):
-        print "HERE"
-        c = np.linalg.norm(np.dot(phi_s.T, (R + gamma * np.dot(phi_s_prime, w_pi) - np.dot(phi_s, w_pi))))/n
-        print c.shape, " AND HERE"
+    R = np.array(sars[:, 2])
+    while(len(I)!=n and c[j] > beta):
+        #c = np.linalg.norm(np.dot(phi_s.T, (R + gamma * np.dot(phi_s_prime, w_pi) - np.dot(phi_s, w_pi))))/n
+        temp = (gamma * np.dot(phi_s_prime, w_pi) - np.dot(phi_s, w_pi))
+        c = np.array([R[i]+temp[i] for i in range(len(R))]).shape
         j = np.argmax(c)
         if c[j] > beta:
-            I.add(j)
-        print phi_s[I].shape
-        w_pi = np.linalg.inverse(np.dot(phi_s[I].T, phi_s[I]) - gamma* np.dot(phi_s[I].T, phi_s_prime[I]))
+            I.add(j)        
+        w_pi = np.linalg.inv(np.dot(phi_s[list(I)].T, phi_s[list(I)]) - gamma* np.dot(phi_s[list(I)].T, phi_s_prime[list(I)]))
         w_pi = np.dot(w_pi, np.dot(phi_s.T, R))
+    print "Finished OMP-TD"
     return w_pi
             
         