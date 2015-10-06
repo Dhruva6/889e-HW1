7dc648cf9f2b95b96b7f59d0a896403895cb10b7
Venkat Rajagopalan <Venkat Rajagopalan>
2015-10-05 21:34:34 -0400

    command line options parsing and sample test file

--- LSPI.py
+++ LSPI.py
@@ -5,9 +5,12 @@
 import csv
 import math
 import random
+import pickle
 import numpy as np
 from sklearn import linear_model
 from Util import generate_sarsa
+from Util import generate_test_states
+from optparse import OptionParser
 
 # the Least Squares Temporal Difference Q learning algorithm
 # Input: sars         - the nx4 array of (s,a,r,s') elements
@@ -17,11 +20,11 @@ from Util import generate_sarsa
 # Output: w_pi - the estimated weight for the linear model
 def LSTDQ(sars, current_pi, gamma = 0.9):
 
-    # at present, our basis is N^d - where d is 9 (features) + 1 (action)
+    # at present, our basis is R^d - where d is 9 (features) + 1 (action)
     k = 10
 
     # to avoid singularities, we start off A with a small delta along the diagonal
-    delta = 1e-03
+    delta = 1e-09
 
     # the "A" Matrix (kxk) 
     A = delta*np.eye(k)
@@ -40,8 +43,9 @@ def LSTDQ(sars, current_pi, gamma = 0.9):
         
         # Update A - here, we add to A, a Rank 1 matrix formed by
         # the vectors phi(s) and (phi(s) - gamma*phi(s'))
-        A = A + np.outer(phi_s, phi_s - gamma*phi_s_prime)
-
+        #A = A + np.outer(phi_s, phi_s - gamma*phi_s_prime)
+        A = A + phi_s * (phi_s - gamma * phi_s_prime).T
+        
         # update B - we add to B, the feature vector scaled by the reward
         b = b + sars[idx,2]*phi_s
 
@@ -68,14 +72,11 @@ def ImprovePolicy(s, w_pi):
         q1 = np.dot(np.append(s[idx],1.0), w_pi)
 
         # update the policy as argmax(action = {0.0, 1.0}) Q^
-        policy[idx] = 1.0 if q0 < q1 else 0.0
+        policy[idx] = 1.0 if q1 > q0 else 0.0
 
         # update the value
         value[idx] = max(q0, q1)
         
-        # to the next state
-        idx = idx+1    
-
     return (policy, value)
 
 # Policy Evaluation at the given states
@@ -89,6 +90,7 @@ def EvaluatePolicy(s, w_pi):
 
     # iterate through every state, 
     for idx in range(0, len(s)):
+
         # the state-action value for action 0.0
         q0 = np.dot(np.append(s[idx,0],0.0), w_pi)
 
@@ -99,11 +101,8 @@ def EvaluatePolicy(s, w_pi):
         value[idx] = max(q0, q1)
 
         # update the policy
-        policy[idx] = True if q0 < q1 else False
+        policy[idx] = True if q1 > q0 else False
         
-        # to the next state
-        idx = idx+1    
-
     return (policy, value)
 
 def LSPI(sars, current_pi, gamma):
@@ -129,7 +128,7 @@ def LSPI(sars, current_pi, gamma):
         new_w_pi = LSTDQ(sars, current_pi, gamma)
 
         # improve the policy
-        new_pi, new_value = ImprovePolicy(sars[:,0], w_pi)
+        new_pi, new_value = ImprovePolicy(sars[:,0], new_w_pi)
 
         # termination condition
         if np.linalg.norm(new_w_pi - w_pi) < eps:
@@ -150,104 +149,171 @@ def LSPI(sars, current_pi, gamma):
 
     return (current_pi, w_pi, current_value)
 
-# prompt
-print "Reading file: generated_episodes_3000.csv"
+#
+# command line options
+#
+parser = OptionParser()
 
-# First read in all the data from the file.
-with open('generated_episodes_3000.csv') as csv_file:
-    data = np.array(list(csv.reader(csv_file))[1:])
+# add the options
+parser.add_option("-v", action="store_true", dest="crossValidateGamma", default=False, help="Run Cross Validation on gamma[default=False]")
+#parser.add_option("-l", action="store_true", dest="loadWeights", help="Load weights from training[default=False]", default=False)
+parser.add_option("-t", action="store_true", dest="testData", help="Test on given data[default=False]", default=False)
 
-# Generate the (s,a,r,s',a') tuple from data
-sarsa = np.array(generate_sarsa(data))
+parser.add_option("-f", action="store", type="string", dest="trainFile", help="CSV Training data file name[default=generated_episodes_3000.csv]", default="generated_episodes_3000.csv")
+parser.add_option("-p", action="store", type="string", dest="paramsFile", help="File with parameters from training[default=params.pk1]", default="params.pk1")
+parser.add_option("-s", action="store", type="string", dest="testFile", help="CSV Test data file name[default=testData.csv]", default="testData.csv")
 
-# pull out the (s,a,r,s) tuple from sarsa
-sars = sarsa[:,0:4]
+# parse the options 
+(options, args) = parser.parse_args()
 
-# should we perform cross validation on gamma?
-crossValidateGamma = True
-#gamma = np.linspace(0.0, 1.0, 50, True)
-gamma = np.linspace(0.95, 1.0, 20, False)
-
-# cross-validate if requested
-if crossValidateGamma:
-
-    # the number of times to run the cross validation for a given gamma
-    maxCVTimes  = 5
-
-    # the number of folds
-    numFolds  = 10
-
-    # number of test elements
-    numTestElements = len(sars)/numFolds
-
-    # number of training elements
-    numTrainElements = len(sars) - numTestElements
-
-    print "Train Elements {}, Test Elements {}".format(numTrainElements, numTestElements)
-
-    # the mean values of each of the policy
-    mean_policy_values = np.zeros((len(gamma),1))
-
-    # index for g
-    gIdx = 0
+#
+#
+#
+# if configured to NOT test
+if options.testData == False:
     
-    # iterate through all the elements of gamma
-    for g in gamma:
+    # prompt
+    print "Reading file: " + options.trainFile
 
-        print "Cross validating for gamma: {0:.3f}".format(g)
-    
-        # the current loop counter
-        cvTimes = 0
+    # First read in all the data from the file.
+    with open(options.trainFile) as csv_file:
+        data = np.array(list(csv.reader(csv_file))[1:])
 
-        # iterate 
-        while cvTimes < maxCVTimes:
+    # Generate the (s,a,r,s',a') tuple from data
+    sarsa = np.array(generate_sarsa(data))
 
-            # get the training set rows
-            trainRows = random.sample(range(0,len(sars)), numTrainElements)
-        
-            # the test set rows
-            testRows = list(set(range(0,len(sars))) - set(trainRows))
-        
-            #  the initial policy executed at s'
-            current_pi = np.reshape(sarsa[:,4], (len(sars),1))
+    # pull out the (s,a,r,s) tuple from sarsa
+    sars = sarsa[:,0:4]
 
-            # LSPI
-            _, w_pi,_ = LSPI(sars[trainRows,:], current_pi, g)
-        
-            # evaluate the policy at sars[testRows,:]
-            _,values = EvaluatePolicy(sars[testRows,0:1], w_pi)
+    # should we perform cross validation on gamma?
+    gamma = np.linspace(0.95, 1.0, 20, False)
 
-            # update the mean_policy_values for the current gamma
-            mean_policy_values[gIdx] = mean_policy_values[gIdx] + np.mean(values)
+    # cross-validate if requested
+    if options.crossValidateGamma == True:
+
+        # the number of times to run the cross validation for a given gamma
+        maxCVTimes  = 5
+
+        # the number of folds
+        numFolds  = 10
+
+        # number of test elements
+        numTestElements = len(sars)/numFolds
+
+        # number of training elements
+        numTrainElements = len(sars) - numTestElements
+
+        print "Train Elements {}, Test Elements {}".format(numTrainElements, numTestElements)
+
+        # the mean values of each of the policy
+        mean_policy_values = np.zeros((len(gamma),1))
+
+        # index for g
+        gIdx = 0
+            
+        # iterate through all the elements of gamma
+        for g in gamma:
+
+            print "Cross validating for gamma: {0:.3f}".format(g)
+            
+            # the current loop counter
+            cvTimes = 0
+
+            # iterate 
+            while cvTimes < maxCVTimes:
+
+                print "now performing CV # {}".format(cvTimes+1)
                 
-            # tick over the counter
-            cvTimes = cvTimes + 1
+                # get the training set rows
+                trainRows = random.sample(range(0,len(sars)), numTrainElements)
+                
+                # the test set rows
+                testRows = list(set(range(0,len(sars))) - set(trainRows))
+                
+                #  the initial policy executed at s'
+                current_pi = np.reshape(sarsa[:,4], (len(sars),1))
 
-        # average over all the cross-validation times
-        mean_policy_values[gIdx,0] = mean_policy_values[gIdx,0]/float(maxCVTimes)
+                # LSPI
+                _, w_pi,_ = LSPI(sars[trainRows,:], current_pi, g)
+
+                # evaluate the policy at sars[testRows,:]
+                _,values = EvaluatePolicy(sars[testRows,0:1], w_pi)
+
+                # update the mean_policy_values for the current gamma
+                mean_policy_values[gIdx] = mean_policy_values[gIdx] + np.mean(values)
+                
+                # tick over the counter
+                cvTimes = cvTimes + 1
+
+            # average over all the cross-validation times
+            mean_policy_values[gIdx,0] = mean_policy_values[gIdx,0]/float(maxCVTimes)
+                
+            # console log
+            print "Mean policy value for test set: {0:.2f}".format(mean_policy_values[gIdx,0])
+                
+            # tick over gIdx
+            gIdx = gIdx + 1
+
+        # write the gamma values to the csv file
+        with open("LSPI_gamma_CV.csv", "w") as out_file:
+            out_file.write("# Gamma, Mean Policy Value\n")
+            for i in range(len(gamma)):
+                out_string = "{0:.5f},{1:.5f}\n".format(gamma[i],mean_policy_values[i,0])
+                out_file.write(out_string)
         
+    else: # use gamma that was picked using the cross validation
+
+        # gamma (from cross validation)
+        gamma = 0.9975
+
         # console log
-        print "Mean policy value for test set: {0:.2f}".format(mean_policy_values[gIdx,0])
+        print "Running LSPI on *ALL* the training data with gamma {0:.3f}".format(gamma)      
+        #  the initial policy executed at s'
+        current_pi = np.reshape(sarsa[:,4], (len(sars),1))
+
+        # LSPI
+        current_pi, w_pi, current_value = LSPI(sars, current_pi, gamma)
+
+        # console log
+        print "Saving gamma and weights to file: " + options.paramsFile
         
-        # tick over gIdx
-        gIdx = gIdx + 1
+        # dump the weight and the gamma to disk
+        paramsOut = open(options.paramsFile, 'wb')
+        pickle.dump(gamma, paramsOut, -1)
+        pickle.dump(w_pi, paramsOut, -1)
+        paramsOut.close()
 
-    # write the gamma values to the csv file
-    with open("LSPI_gamma_CV.csv", "w") as out_file:
-        out_file.write("# Gamma, Mean Policy Value\n")
-        for i in range(len(gamma)):
-            out_string = "{0:.5f},{1:.5f}\n".format(gamma[i],mean_policy_values[i,0])
-            out_file.write(out_string)
+        # # save a sample of test data
+        # writer = csv.writer(open(options.testFile, 'w'))
+        # for row in range(0, len(sars)):
+        #     writer.writerow(sars[row,0])
+else :
 
-else: # just pick the gamma with the largest return
+    print "Loading gamma and w_pi from file: " + options.paramsFile
+    
+    # load the gamma and the weights from paramsFile
+    paramsIn = open(options.paramsFile, 'rb')
+    gamma = pickle.load(paramsIn)
+    w_pi = pickle.load(paramsIn)
+    paramsIn.close()
+    
+    # load the scaler
+    scalerIn = open('Scaler.pk1', 'rb')
+    scaler = pickle.load(scalerIn)
+    scalerIn.close()
+   
+    # load the file with test data
+    print "Loading test data from file: " + options.testFile
+    with open(options.testFile) as csv_file:
+        data = np.array(list(csv.reader(csv_file))[1:])
 
-    # gamma (from cross validation)
-    gamma = 0.9975
+    # generate the test states from data
+    test_s = np.array(generate_test_states(data, scaler))
 
-    #  the initial policy executed at s'
-    current_pi = np.reshape(sarsa[:,4], (len(sars),1))
+    # evaluate the policy
+    policy, value = EvaluatePolicy(test_s, w_pi)
 
-    # LSPI
-    current_pi, w_pi, current_value = LSPI(sars, current_pi, gamma)
 
-    print "gamma: {0:.5f}, Mean Policy Value: {1:.5f}".format(gamma, np.mean(current_value))
+    
+
+
